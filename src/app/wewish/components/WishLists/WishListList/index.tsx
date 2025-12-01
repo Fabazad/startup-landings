df864a8b4eacd281e323853cbaec72c7b179b6e2
Fabazad <fabien.turgut@gmail.com>
2025-12-01 23:14:34 +0100

    list skeletons

--- src/app/wewish/components/WishLists/WishListList/index.tsx
+++ src/app/wewish/components/WishLists/WishListList/index.tsx
@@ -4,6 +4,7 @@ import { Button } from "@mui/material";
 import { Iconify } from "src/components/iconify";
 import { WishList } from "../../../types/WishList";
 import { WishListItem } from "./WishListItem";
+import { WishListItemSkeleton } from "./WishListItemSkeleton";
 
 export const WishListList = ({ wishLists, isLoading, emptyContent, href }: {
     wishLists: Array<WishList>,
@@ -11,28 +12,23 @@ export const WishListList = ({ wishLists, isLoading, emptyContent, href }: {
     href?: (listId: number) => string,
     emptyContent: { button?: { title: string; href: string; }; title: string; }
 }) => {
-    return (
-        <Box>
-            {wishLists.length === 0 && (
-                <EmptyContent title={emptyContent.title} action={
-                    <Button href={emptyContent.button?.href} variant="contained" sx={{ borderRadius: 999, px: 2, mt: 1 }} size="large">
-                        <Iconify icon="material-symbols:add" sx={{ mr: 1 }} />
-                        {emptyContent.button?.title}
-                    </Button>
-                }
-                />
-            )}
-            {wishLists?.length !== 0 && (
-                <>
-                    <Box sx={{ mt: 2 }}>
+    if (!isLoading && wishLists.length === 10) return (
+        <EmptyContent title={emptyContent.title} action={emptyContent.button && (
+            <Button href={emptyContent.button?.href} variant="contained" sx={{ borderRadius: 999, px: 2, mt: 1 }} size="large">
+                <Iconify icon="material-symbols:add" sx={{ mr: 1 }} />
+                {emptyContent.button?.title}
+            </Button>
+        )}
+        />
+    )
 
-                        <Box sx={{ mb: 2, display: 'flex', flexDirection: 'column', gap: 2 }}>
-                            {wishLists?.map((list) => (
-                                <WishListItem key={list.id} wishList={list} href={href ? href(list.id) : undefined} />
-                            ))}
-                        </Box>
-                    </Box>
-                </>
+    return (
+        <Box sx={{ my: 2, display: 'flex', flexDirection: 'column', gap: 2 }}>
+            {isLoading && <WishListItemSkeleton />}
+            {!isLoading && wishLists.length > 0 && (
+                wishLists.map((list) => (
+                    <WishListItem key={list.id} wishList={list} href={href ? href(list.id) : undefined} />
+                ))
             )}
         </Box>
     )
