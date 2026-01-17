aee33dfd8c7c597d07acaa4c85653d7d715eaa81
Fabazad <fabien.turgut@gmail.com>
2026-01-17 19:37:09 +0100

    fix straight redirect on /evny/

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,17 +1,9 @@
 import { LandingView } from 'src/sections/landing/view';
-import { getProductIdea } from '../getProductIdea';
-import { redirect } from 'next/navigation';
-import { paths } from 'src/routes/paths';
 
 // Revalidate this page every hour (ISR)
 export const revalidate = 3600;
 
 export default async function Page() {
-
-  const { isReady } = await getProductIdea()
-
-  if (isReady) return redirect(paths.envy.root);
-
 
   return <LandingView />;
 }
